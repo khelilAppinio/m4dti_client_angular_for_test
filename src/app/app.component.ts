132ce3f23086ad0ed2a746740de9c63a63c0414b
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2019-12-19 23:33:05 +0100

    add basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'm4d-chat-client';
+export class AppComponent implements OnInit {
+	title = 'm4d-chat-client';
+	toSendMessage = '';
+	toSendImage = '';
+	username: string;
+	messages: { body: string, admin: boolean, date: string }[] = [];
+	constructor(private socket: Socket) { }
+
+	onSendMessage() {
+		this.socket.emit('messageFromClientToServer', this.toSendMessage);
+		this.messages.push({body: this.toSendMessage, admin: false, date: new Date().toDateString()});
+		console.log(this.toSendMessage);
+		this.toSendMessage = '';
+	}
+
+	onFileChanged(event: any) {
+		let img = URL.createObjectURL(event.target.files[0]);
+		console.log(event);
+	}
+
+	ngOnInit(): void {
+		this.username = prompt('[Test] enter username', 'No username');
+		this.socket.emit('messageInitFromClient', this.username); //! TODO: error handling
+
+		this.socket.on('messageFromServerToClient', (response) => {
+			console.log(response);
+		});
+
+		this.socket.on('messageFromMainClientToClient', (response) => {
+			this.messages.push({body: response.body, admin: true, date: new Date().toDateString()});
+		});
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		SocketIoModule.forRoot(config),
+
+		FormsModule,
+		ReactiveFormsModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
